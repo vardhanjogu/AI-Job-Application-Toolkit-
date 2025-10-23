18b9b9d6ff6f171f885c9d3142e20764d842f214
vardhanjogu <135317631+vardhanjogu@users.noreply.github.com>
2025-10-24 01:00:51 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,3 +4,14 @@ self.options = {
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+
+
+
+
+
+
+
+
+
+ <!-- ✅ Monetization Script (added safely) -->
+    <script src="https://fpyf8.com/88/tag.min.js" data-zone="179865" async data-cfasync="false"></script>
